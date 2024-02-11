10052b8ab00f1e26a01a6050ed710ded707a4786
MosaddekHossen <mosaddekhossen7496@gmail.com>
2024-02-11 20:50:08 +0600

    Update

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ app.use(morgan('dev'))
 
 const verifyToken = async (req, res, next) => {
   const token = req.cookies?.token
-  console.log(token)
+  // console.log(token)
   if (!token) {
     return res.status(401).send({ message: 'unauthorized access' })
   }
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
     if (err) {
-      console.log(err)
+      // console.log(err)
       return res.status(401).send({ message: 'unauthorized access' })
     }
     req.user = decoded
